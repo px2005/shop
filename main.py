99bf54e1d6b85640c9c43f3f6d9f35b665a51780
Александр Горыничев <px2005@yandex.ru>
2022-08-25 13:34:21 +0300

    shop v.1.01

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'Товар: {self.title}'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        if title == '' or price == '':
+            return "Ошибка проверь поля"
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка проверь поля"
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
